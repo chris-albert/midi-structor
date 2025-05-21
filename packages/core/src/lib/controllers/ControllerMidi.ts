0a8f305fa2a3687e9b215df3d3c0ca1defe9db45
Chris Albert <christopher.albert@mlb.com>
2025-05-20 19:18:52 -0700

    Back to normal

--- packages/core/src/lib/controllers/ControllerMidi.ts
+++ packages/core/src/lib/controllers/ControllerMidi.ts
@@ -1,7 +1,7 @@
 import { ReactControllersApp } from './ReactControllersApp'
 import ControllerWorker from '../workers/controller/worker?worker'
 
-const USE_WORKER = true
+const USE_WORKER = false
 
 const usingWorker = () => {
   console.log('Loading Worker Controller...')
@@ -14,7 +14,6 @@ const usingLocal = () => {
 }
 
 const ControllerListener = () => {
-  console.log('USE_WORKER', USE_WORKER)
   const _ = USE_WORKER ? usingWorker() : usingLocal()
 }
 
