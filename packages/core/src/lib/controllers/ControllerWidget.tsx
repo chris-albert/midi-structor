bdcd741e0fe345745a145994fa4a40f8b715da8e
Chris Albert <christopher.albert@mlb.com>
2025-05-27 16:18:52 -0700

    checkpoint before widget refactor

--- packages/core/src/lib/controllers/ControllerWidget.tsx
+++ packages/core/src/lib/controllers/ControllerWidget.tsx
@@ -7,55 +7,135 @@ const TargetSchema = Schema.Struct({
 })
 
 type TargetSchema = typeof TargetSchema.Type
+type TargetSchemaFields = typeof TargetSchema.fields
 
 const TargetsSchema = Schema.Struct({
-  target: Schema.Array(MidiTarget.Schema),
+  targets: Schema.Array(MidiTarget.Schema),
 })
 
 type TargetsSchema = typeof TargetsSchema.Type
+type TargetsSchemaFields = typeof TargetsSchema.fields
+
+type WidgetInput = MidiTarget | Array<MidiTarget>
 
 export type ControllerWidget<
   K extends SchemaAST.LiteralValue = any,
-  A extends Schema.Struct.Fields = any
+  A extends Schema.Struct.Fields = any,
+  In extends WidgetInput = any
 > = {
   name: K
   schema: Schema.TaggedStruct<K, A>
-  targets: (a: Schema.Struct.Type<A>) => Array<MidiTarget>
+  targets: (a: Schema.Struct.Type<A>) => In
   component: (a: Schema.Struct.Type<A>) => React.ReactElement
   tracks: (a: Schema.Struct.Type<A>) => Array<string>
-  // init: () => Schema.Struct.Type<A>
+  init?: (i: In) => Schema.Struct.Type<A>
 }
 
 export type ControllerWidgetProps<
+  K extends SchemaAST.LiteralValue = any,
+  A extends Schema.Struct.Fields = any,
+  In extends WidgetInput = any
+> = {
+  name: K
+  schema: Schema.Struct<A>
+  targets: (a: Schema.Struct.Type<A>) => In
+  component: (a: Schema.Struct.Type<A>) => React.ReactElement
+  tracks?: (a: Schema.Struct.Type<A>) => Array<string>
+  init?: (i: In) => Schema.Struct.Type<A>
+}
+
+const of = <
+  K extends SchemaAST.LiteralValue,
+  A extends Schema.Struct.Fields,
+  In extends WidgetInput
+>(
+  props: ControllerWidgetProps<K, A, In>
+): ControllerWidget<K, A, In> => ({
+  name: props.name,
+  schema: Schema.TaggedStruct(props.name, props.schema.fields),
+  targets: props.targets,
+  component: props.component,
+  tracks: props.tracks || (() => []),
+  init: props.init,
+})
+
+/**
+ * Controller Widget with a single target
+ */
+
+export type ControllerWidgetPropsOne<
   K extends SchemaAST.LiteralValue = any,
   A extends Schema.Struct.Fields = any
 > = {
   name: K
   schema: Schema.Struct<A>
-  targets: (a: Schema.Struct.Type<A>) => Array<MidiTarget>
-  component: (a: Schema.Struct.Type<A>) => React.ReactElement
-  // init: () => Schema.Struct.Type<A>
-  tracks?: (a: Schema.Struct.Type<A>) => Array<string>
+  component: (
+    a: Schema.Struct.Type<A & TargetSchemaFields>
+  ) => React.ReactElement
+  init: () => Schema.Struct.Type<A>
+  tracks?: (a: Schema.Struct.Type<A & TargetSchemaFields>) => Array<string>
 }
 
-const of = <K extends SchemaAST.LiteralValue, A extends Schema.Struct.Fields>(
-  props: ControllerWidgetProps<K, A>
-): ControllerWidget<K, A> => ({
+const one = <K extends SchemaAST.LiteralValue, A extends Schema.Struct.Fields>(
+  props: ControllerWidgetPropsOne<K, A>
+): ControllerWidget<K, A & TargetSchemaFields, MidiTarget> => ({
   name: props.name,
-  schema: Schema.TaggedStruct(props.name, props.schema.fields),
-  // init: props.init,
-  targets: props.targets,
+  schema: Schema.TaggedStruct(props.name, {
+    ...TargetSchema.fields,
+    ...props.schema.fields,
+  }) as Schema.TaggedStruct<K, A & TargetSchemaFields>,
+  targets: (w: any) => w.target,
   component: props.component,
   tracks: props.tracks || (() => []),
+  init: (target) => ({
+    target,
+    ...(props.init() as any),
+  }),
+})
+
+/**
+ * Controller Widget with many targets
+ */
+
+export type ControllerWidgetPropsMany<
+  K extends SchemaAST.LiteralValue = any,
+  A extends Schema.Struct.Fields = any
+> = {
+  name: K
+  schema: Schema.Struct<A>
+  component: (
+    a: Schema.Struct.Type<A & TargetsSchemaFields>
+  ) => React.ReactElement
+  init: () => Schema.Struct.Type<A>
+  tracks?: (a: Schema.Struct.Type<A & TargetsSchemaFields>) => Array<string>
+}
+
+const many = <K extends SchemaAST.LiteralValue, A extends Schema.Struct.Fields>(
+  props: ControllerWidgetPropsMany<K, A>
+): ControllerWidget<K, A & TargetsSchemaFields, Array<MidiTarget>> => ({
+  name: props.name,
+  schema: Schema.TaggedStruct(props.name, {
+    ...TargetsSchema.fields,
+    ...props.schema.fields,
+  }) as Schema.TaggedStruct<K, A & TargetsSchemaFields>,
+  targets: (w: any) => w.targets,
+  component: props.component,
+  tracks: props.tracks || (() => []),
+  init: (targets) => ({
+    targets,
+    ...(props.init() as any),
+  }),
 })
 
 const intersect = <
   K extends SchemaAST.LiteralValue,
   A extends Schema.Struct.Fields,
-  B extends Schema.Struct.Fields
+  B extends Schema.Struct.Fields,
+  In extends WidgetInput
 >(
   widget: ControllerWidget<K, A>,
   bSchema: Schema.Struct<B>
+  // bInit: (i: In) => Schema.Struct.Type<B>
 ): ControllerWidget<K, A & B> =>
   of({
     name: widget.name,
@@ -63,17 +143,22 @@ const intersect = <
       ...widget.schema.fields,
       ...bSchema.fields,
     }) as Schema.Struct<A & B>,
-    // init: widget.init as () => Schema.Struct.Type<A & B>,
     targets: (f) => widget.targets(f as Schema.Struct.Type<A>),
     component: (f) => widget.component(f as Schema.Struct.Type<A>),
     tracks: (f) =>
       widget.tracks !== undefined
         ? widget.tracks(f as Schema.Struct.Type<A>)
         : [],
+    // init: (input) => ({
+    //   ...(widget.init(input) as any),
+    //   ...(bInit(input) as any),
+    // }),
   })
 
 export const ControllerWidget = {
   of,
+  one,
+  many,
   intersect,
 }
 
--- packages/core/src/lib/controllers/widgets/PlayWidget.tsx
+++ packages/core/src/lib/controllers/widgets/PlayWidget.tsx
@@ -6,7 +6,28 @@ import { TX_MESSAGE } from '../../project/AbletonUIMessage'
 import { ControllerWidget } from '../ControllerWidget'
 import { Schema } from 'effect'
 
-export const PlayWidget = ControllerWidget.of({
+export const PlayWidget = ControllerWidget.one({
+  name: 'play',
+  schema: Schema.Struct({
+    color: Color.Schema,
+  }),
+  init: () => ({
+    color: Color.GREEN,
+  }),
+  component: ({ target, color }) => {
+    const dawEmitter = Midi.useDawEmitter()
+
+    return (
+      <pad
+        color={color}
+        target={target}
+        onClick={() => dawEmitter.send(TX_MESSAGE.play())}
+      />
+    )
+  },
+})
+
+export const PlayWidget1 = ControllerWidget.of({
   name: 'play',
   schema: Schema.Struct({
     target: MidiTarget.Schema,
